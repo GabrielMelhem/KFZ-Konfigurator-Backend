59f26eb349d2ada66aacf7ca06725e9f35dc72a8
GabrielMelhem <gabrielmelhem@gmail.com>
2024-03-08 18:12:55 +0100

    add a branch github

--- src/main/java/com/example/carconfigurator/FelgenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.carconfigurator;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FelgenRepository extends JpaRepository<Felgen, Long> {
-
-}
